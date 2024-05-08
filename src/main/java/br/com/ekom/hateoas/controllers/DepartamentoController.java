17c68ca5ecaa30d313d8102b5c305672272f9e61
aopaixao <aopaixao@outlook.com>
2024-05-08 11:09:23 -0300

    Implementação do hateoas

--- src/main/java/br/com/ekom/hateoas/controllers/DepartamentoController.java
+++ src/main/java/br/com/ekom/hateoas/controllers/DepartamentoController.java
@@ -1,8 +1,14 @@
 package br.com.ekom.hateoas.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,12 +30,40 @@ public class DepartamentoController {
 	@Autowired
 	DepartamentoService departamentoService;
 
+	/*
 	@GetMapping
 	public ResponseEntity<List<Departamento>> findAll(){
 		return new 
 				ResponseEntity<>(departamentoService.findAll(), HttpStatus.OK);
 	}
+	*/
+	
+	@GetMapping
+	ResponseEntity<CollectionModel<EntityModel<Departamento>>> findAll() {
 
+		List<EntityModel<Departamento>> departamentos = StreamSupport.stream(departamentoService.findAll().spliterator(), false)
+			.map(departamento -> EntityModel.of(departamento,
+				linkTo(methodOn(DepartamentoController.class).findById(departamento.getDepartamentoId())).withSelfRel(),
+				linkTo(methodOn(DepartamentoController.class).findAll()).withRel("departamentos")))
+			.collect(Collectors.toList());
+
+		return ResponseEntity.ok(
+			CollectionModel.of(departamentos,
+				linkTo(methodOn(DepartamentoController.class).findAll()).withSelfRel()));
+	}
+	
+	@GetMapping("/{id}")
+	ResponseEntity<EntityModel<Departamento>> findById(@PathVariable Long id) {
+
+		return departamentoService.findById(id)
+				.map(departamento -> EntityModel.of(departamento, 
+						linkTo(methodOn(DepartamentoController.class).findById(departamento.getDepartamentoId())).withSelfRel(), 
+						linkTo(methodOn(EmployeeController.class).findAll()).withRel("departamentos"))) 
+				.map(ResponseEntity::ok) //
+				.orElse(ResponseEntity.notFound().build());
+	}	
+
+	/*
 	@GetMapping("/{id}")
 	public ResponseEntity<Departamento> findById(@PathVariable Long id) {
 		Departamento departamento = departamentoService.findById(id);
@@ -43,6 +77,7 @@ public class DepartamentoController {
 					ResponseEntity<>(departamento, 
 							HttpStatus.OK);
 	}
+	*/
 	
 	@PostMapping
 	public ResponseEntity<Departamento> save(@RequestBody Departamento departamento) {
